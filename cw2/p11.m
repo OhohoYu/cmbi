9b89f2a93b1fe4ffe01d8ee7471be868ac501dae
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2015-02-27 12:30:51 +0000

    added cw2 report

--- cw2/p11.m
+++ cw2/p11.m
@@ -3,22 +3,25 @@ function p11()
 SAMPLE_SIZE = 25;
 MU0 = 1;
 MU1 = 1.5;
+MUError = 0;
+STD_DEV = 0.25;
 
 % set seed for random generator
 rng(1);
 
-MUError = 0;
-STD_DEV = 0.25;
-
+% compute the new Y
 Y0 = MU0 + MUError + STD_DEV .* randn(SAMPLE_SIZE, 1);
 Y1 = MU1 + MUError + STD_DEV .* randn(SAMPLE_SIZE, 1);
 
+% estimate the new means
 muEst0 = mean(Y0);
 muEst1 = mean(Y1);
 
+% estimate the new std deviations
 stdDevEst0 = std(Y0);
 stdDevEst1 = std(Y1);
 
+% check that they are close to the true values
 tol = 0.1;
 assert(abs(muEst0 - MU0) < tol);
 assert(abs(muEst1 - MU1) < tol);
@@ -27,15 +30,28 @@ assert(abs(stdDevEst1 - STD_DEV) < tol);
 
 %% b
 
-H = ttest2(Y0, Y1)
+% apply t-test, H should be 1
+[H,P,CI,STATS] = ttest2(Y0, Y1);
+
 % null should be rejected, the samples come from different distributions
 assert(H == 1); 
 
 %% c 
 
-% i 
+% build matrices X and Y
+X = [repmat([1 0], SAMPLE_SIZE,1); repmat([0 1], SAMPLE_SIZE,1)];
 
-X = [repmat([1 0], SAMPLE_SIZE); repmat([0 1], SAMPLE_SIZE)];
+Y = [Y0;Y1];
+
+partC(SAMPLE_SIZE, X, Y)
+
+end
+
+function partC(SAMPLE_SIZE, X, Y)
+
+% i 
+M = X*inv(X'*X)*X';
+
 
 
 
