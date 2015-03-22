b9c550ef4e1c7ff9b6042dcd3581045c79ead238
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2015-03-22 22:47:05 +0000

    finished the deformation field, starting landmark error

---
+++ project/code/c3Task3.m
@@ -0,0 +1,53 @@
+function c3Task3()
+
+addpath NIFTI_20110921
+
+full_vol = load_nii('../data/4DCT_ref_lungs.nii');
+Dpred = load('c3CrossValidD3.mat');
+[COUCH, ~, X, Y, Z, T, D] = size(Dpred.Dpred3);
+clear Dpred
+MODELS = 3;
+CPG_INCOMPL = 10; % only 10 out of 40 CT scans are provided
+error_mean = zeros(MODELS, COUCH, CPG_INCOMPL);
+error_std_dev = zeros(MODELS, COUCH, CPG_INCOMPL);
+
+for m=1:3
+  m = 3;
+  dpred_var_name = sprintf('Dpred%d',m );
+  data_file_name = sprintf('c3CrossValidD%d.mat', m);
+  Dpred = load(data_file_name);
+  Dpred = Dpred.(dpred_var_name);
+
+  for couch=1:COUCH
+    couch
+    for cpg=1:CPG_INCOMPL
+      
+
+      reg_filename = sprintf('../data/%dpos_data_couch/reg_couch_pos%d_cine%.2d_cpg.nii',couch,couch,cpg-1);
+      reg_cp = load_nii(reg_filename);
+
+      trans_nii = reg_cp;
+      trans_nii.img = reshape(Dpred(couch, cpg,:,:,:,:,:), [ X, Y, Z, T, D]);
+
+      [~, def_field_trans, ~] = deformNiiWithCPG(trans_nii,full_vol,full_vol);
+      [~, def_field_reg, ~] = deformNiiWithCPG(reg_cp,full_vol,full_vol);
+
+      cine_ct_filename = sprintf('../data/%dpos_data_couch/ct_couch_pos%d_cine%.2d.nii',couch,couch,cpg-1);
+      cine_ct = load_nii(cine_ct_filename);
+      cine_ct = cine_ct.img ~= -1;
+
+      diff = sqrt(sum((def_field_trans.img - def_field_reg.img).^2 ,5));
+
+      diff = diff .* cine_ct; % filter all the voxels outside the lungs
+
+      error_mean(m,couch, cpg) = mean(diff(:));
+      error_std_dev(m,couch, cpg) = std(diff(:));
+    end
+  end
+
+  clear Dpred
+end
+
+save('def_field_error.mat', 'error_mean', 'error_std_dev');
+
+end
--- project/code/c3VisualAss.m
+++ project/code/c3VisualAss.m
@@ -13,22 +13,20 @@ full_vol = load_nii('../data/4DCT_ref_lungs.nii');
   
   [COUCH, X,Y,Z,T,D]
   
-  couch = 1; cpg = 10;
+  couch = 1; cpg = 8;
   reg_filename = sprintf('../data/%dpos_data_couch/reg_couch_pos%d_cine%.2d_cpg.nii',couch,couch,cpg);
   reg_cp = load_nii(reg_filename);
+  
+  cine_ct_filename = sprintf('../data/%dpos_data_couch/ct_couch_pos%d_cine%.2d.nii',couch,couch,cpg);
+  cine_ct = load_nii(cine_ct_filename);
+  
   
   trans_nii = reg_cp;
   trans_nii.img = reshape(Dpred(couch, cpg,:,:,:,:,:), [ X, Y, Z, T, D]);
   
-  [def_vol_nii, def_field_nii, dis_field_nii] = deformNiiWithCPG(trans_nii,full_vol,full_vol,'true');
-  
-  %dispNiiSlice(def_vol_nii, 'z', 70);
-  dispNiiSlice(trans_nii, 'z', 70);
-  
-  %figure(1)
-  %dispNiiSliceColourOverlay(cp, trans_nii, 'z', 70);
-  
-  
+  [def_vol_nii, def_field_nii, dis_field_nii] = deformNiiWithCPG(trans_nii,full_vol,full_vol);
+
+  dispNiiSliceColourOverlay(cine_ct, def_vol_nii, 'z', 70);
   
   clear Dpred
 %end
