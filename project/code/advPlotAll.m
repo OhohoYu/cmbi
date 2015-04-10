a6a73c9df620289af4e60e5f9eabec2acfdaa0ac
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2015-04-10 12:33:32 +0100

    added def field plots and visualAss plots

--- project/code/advPlotAll.m
+++ project/code/advPlotAll.m
@@ -12,11 +12,15 @@ for c=1:COUCH
     hold on
   end
 
-  legend('linear', 'quadratic', 'cubic', 'linear - phase separation', ...
+  set(h, 'Position', [0 0 600 400]);
+  h_legend = legend('linear', 'quadratic', 'cubic', 'linear - phase separation', ...
   'quadratic - phase separation', 'cubic - phase separation', 'B-spline', ...
-  '2D linear', '2D quadratic', 'Location', 'northoutside')
+  '2D linear', '2D quadratic', 'Location', 'northoutside');
+  set(h_legend,'Box','off');
+  set(gca,'FontSize',13,'FontName','cmr');
   xlabel('Cine CT volume');
-  ylabel('Deformation error - Mean');
+  ylabel('Error mean');
+  axis tight
   figname = sprintf('../report/figures/task4/def_mean_error_couch%d.eps', c);
   hgexport(h, figname);
 end
@@ -28,11 +32,15 @@ for c=1:COUCH
     hold on
   end
 
-  legend('linear', 'quadratic', 'cubic', 'linear - phase separation', ...
+  set(h, 'Position', [0 0 600 400]);
+  h_legend = legend('linear', 'quadratic', 'cubic', 'linear - phase separation', ...
   'quadratic - phase separation', 'cubic - phase separation', 'B-spline', ...
-  '2D linear', '2D quadratic', 'Location', 'northoutside')
+  '2D linear', '2D quadratic', 'Location', 'northoutside');
+  set(h_legend,'Box','off');
+  set(gca,'FontSize',13,'FontName','cmr');
   xlabel('Cine CT volume');
-  ylabel('Deformation error - Std. dev.');
+  ylabel('Error std-dev');
+  axis tight
   figname = sprintf('../report/figures/task4/def_stddev_error_couch%d.eps', c);
   hgexport(h, figname);
 end
--- project/code/c3VisualAss.m
+++ project/code/c3VisualAss.m
@@ -2,33 +2,38 @@ function c3VisualAss()
 
 addpath NIFTI_20110921
 
-
+MODELS = 1:9;
+axis_limits = [45, 355, 45, 275];
 
 full_vol = load_nii('../data/4DCT_ref_lungs.nii');
-%for m=1:3
+for m=MODELS
   
-  Dpred = load('c3CrossValidD3.mat');
-  Dpred = Dpred.Dpred3;
+  dpred_var_name = sprintf('Dpred%d',m );
+  data_file_name = sprintf('c3CrossValidD%d.mat', m);
+  Dpred = load(data_file_name);
+  Dpred = Dpred.(dpred_var_name);
+  
   [COUCH, CPG, X, Y, Z, T, D] = size(Dpred);
   
-  [COUCH, X,Y,Z,T,D]
-  
-  couch = 1; cpg = 8;
+  couch = 1; cpg = 1;
   reg_filename = sprintf('../data/%dpos_data_couch/reg_couch_pos%d_cine%.2d_cpg.nii',couch,couch,cpg);
   reg_cp = load_nii(reg_filename);
   
   cine_ct_filename = sprintf('../data/%dpos_data_couch/ct_couch_pos%d_cine%.2d.nii',couch,couch,cpg);
   cine_ct = load_nii(cine_ct_filename);
   
-  
   trans_nii = reg_cp;
   trans_nii.img = reshape(Dpred(couch, cpg,:,:,:,:,:), [ X, Y, Z, T, D]);
   
   [def_vol_nii, def_field_nii, dis_field_nii] = deformNiiWithCPG(trans_nii,full_vol,full_vol);
 
-  dispNiiSliceColourOverlay(cine_ct, def_vol_nii, 'z', 70);
-  
+  h = figure(1);
+  dispNiiSliceColourOverlay(cine_ct, def_vol_nii, 'z', 66);
+  axis(axis_limits);
+  figname = sprintf('../report/figures/task4/visAss_m%d.png', m);
+  saveas(h, figname, 'png');
+      
   clear Dpred
-%end
+end
 
 end
